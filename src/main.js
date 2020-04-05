7789f05ed6adeb87c7bf865f4b8c7d38bbd81949
Wesley Da Silva Felix <wesley.felix@teleperformance.com.br>
2020-04-05 19:24:48 -0300

    Buscando da API

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
+import api from './api';
+
 class app {
     constructor(){
         this.repositories= [];
 
         this.formEl = document.getElementById('repo-form');
+        this.inputEl = document.querySelector('input[name=repository]');
         this.listEl = document.getElementById('repo-list');
 
         this.registerHandlers();
@@ -12,15 +15,29 @@ class app {
         this.formEl.onsubmit = event => this.addRepository(event);
     }
 
-    addRepository(event) {
+    async addRepository(event) {
         event.preventDefault();
 
+        const repoInput = this.inputEl.value;
+
+        if (repoInput.length === 0)
+            return;
+
+        
+        const response = await api.get(`/repos/${repoInput}`);
+        
+        const {name, description, html_url, owner:{avatar_url} } = response.data;
+
+        
+
         this.repositories.push({
-            name: 'rocketseat.com.br',
-            description:'Tire a sua ideia do papel e dê vida a sua startup.',
-            avatar_url: 'https://avatars0.githubusercontent.com/u/28929274?v=4',
-            html_url:'https://github.com/wfelix11/Projeto_final_ES6'
-        })
+            name,
+            description,
+            avatar_url,
+            html_url
+        });
+
+        this.inputEl.value = '';
 
         this.render()
     }
@@ -40,7 +57,7 @@ class app {
 
             let linkEl = document.createElement('a');
             linkEl.setAttribute('target', '_blank');
-            //linkEl.setAttribute('href', repo.html_url);
+            linkEl.setAttribute('href', repo.html_url);
             linkEl.appendChild(document.createTextNode('Acessar'));
 
             let listItemEl = document.createElement('li');
